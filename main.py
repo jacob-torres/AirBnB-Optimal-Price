7e1b396de67fe007957ad8dfad00055fe6ebae83
Jacob Torres <contact@jacobtorres.net>
2026-06-07 14:59:23 -0600

    Updated dependencies and fixed bugs in UI, and dead code.

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 """Instantiates the Airbnb application."""
-import pandas as pd
 import numpy as np
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, request
 from tensorflow.keras.models import load_model
-from pathlib import Path
 
 app = Flask(__name__)
 
 # Import Neural Network model
 model = load_model(filepath="./models/airbnbpredict_all_data.h5")
-# Define the column names for importing the user data
+# Define the column names in the exact order the model was trained on,
+# matching the `name` attributes of the inputs in home.html
 cols = ['latitude', 'longitude', 'neighbourhood', 'room_type',
         'minimum_nights', 'number_of_reviews',
         'calculated_host_listings_count', 'availability_365']
 
 
 @app.route('/')
-async def index():
+def index():
     """Homepage view."""
     return render_template('home.html')
 
@@ -24,18 +23,23 @@ async def index():
 @app.route('/predict', methods=['POST'])
 def predict():
     """Get the information from the home.html and returns the predicted value."""
-    # Collect the user data and change to array and then to DataFrame
-    user_data = [float(x) for x in request.form.values()]
-    user_data_np = np.array(user_data)
-    user_data_df = pd.DataFrame([user_data_np], columns=[cols])
-    # Use model to make prediction
-    prediction = model.predict(user_data_df)
+    # Collect the user data in the model's expected column order
+    try:
+        user_data = [float(request.form[col]) for col in cols]
+    except (KeyError, ValueError):
+        return render_template('home.html',
+                               pred='Please enter a valid number for every field.')
 
-    if prediction < 0:
+    user_data_np = np.array([user_data])
+    # Use model to make prediction
+    prediction = model.predict(user_data_np)
+    predicted_price = prediction[0][0]
+
+    if predicted_price < 0:
         return render_template('home.html',
                                pred='Your set parameters are out of range something trained model has not seen!')
 
-    elif prediction > 2000000:
+    elif predicted_price > 2000000:
         return render_template('home.html',
                                pred='Your set parameters are out of range something! Please revisit them.')
     else:
@@ -43,7 +47,7 @@ def predict():
                                pred='Your property should be listed at {} in the '
                                     'local currency of the geo '
                                     "location. "
-                                    .format(int(prediction[0][0])))
+                                    .format(int(predicted_price)))
 
 if __name__ == '__main__':
     app.run(debug=True)
